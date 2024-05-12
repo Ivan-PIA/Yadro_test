2300221b7addbf67aefbb856d7039e80c7495e2c
Ivan-PIA <wind.vanya@gmail.com>
2024-05-13 02:22:47 +0700

    class add gaussian noise

---
+++ src/Gaussian_noise.h
@@ -0,0 +1,38 @@
+#include <complex>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+class Gaussian_noise{
+
+    private:
+        double real;
+        double imag;
+
+    public:
+        vector<complex<double>> add_Gaussian_noise(vector<complex<double>> vec_qam, double dispersion){
+            
+            random_device rd;
+            mt19937 gen(rd());
+            normal_distribution<double> d(0.0, dispersion);
+
+            for (int i = 0 ; i < vec_qam.size(); i++){
+                
+                imag = vec_qam[i].imag();
+                real = vec_qam[i].real();
+
+                imag += d(gen);
+                real += d(gen);
+
+                vec_qam[i] = complex<double>(real, imag);
+                
+
+            }
+            return vec_qam;
+            
+        }
+
+};
